8c6ad5cf7d53a724547cb5f377a37dc845fd321a
Ashish Doneriya <ashishdoneriya@gmail.com>
2026-05-27 03:03:11 +0530

    Update project to Java 25 and Spring Boot 4.0.6

--- src/main/java/com/csetutorials/vuedisk/VueDiskApplication.java
+++ src/main/java/com/csetutorials/vuedisk/VueDiskApplication.java
@@ -4,14 +4,7 @@ import com.csetutorials.vuedisk.services.CommandLineArgumentsParser;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.task.TaskExecutionAutoConfiguration;
-import org.springframework.boot.web.embedded.tomcat.TomcatProtocolHandlerCustomizer;
-import org.springframework.context.annotation.Bean;
-import org.springframework.core.task.AsyncTaskExecutor;
-import org.springframework.core.task.support.TaskExecutorAdapter;
 import org.springframework.scheduling.annotation.EnableAsync;
-
-import java.util.concurrent.Executors;
 
 @SpringBootApplication
 @Log4j2
